7e7603bd0bb3bc51d3a94e431c40e311efd55015
Anime Quatroctus <kanadetachibona@gmail.com>
2016-08-14 12:25:19 -0700

    Molecule system. Updated all Item classes to use new molecule system.

--- src/java/com/messorix/moleculecraft/base/items/ModItem.java
+++ src/java/com/messorix/moleculecraft/base/items/ModItem.java
@@ -1,21 +1,16 @@
 package com.messorix.moleculecraft.base.items;
 
-import com.messorix.moleculecraft.base.ModAtoms;
 import com.messorix.moleculecraft.base.MoleculecraftBase;
-import com.messorix.moleculecraft.base.classes.ModMolecule;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ModItem extends Item
 {
-	public String NAME;
-	public ModMolecule MOLECULE = (ModMolecule) new ModMolecule().setAmount(0);
-	public ModAtoms modAtoms = new ModAtoms();
-	
+	public String name;
     public ModItem(String unlocalizedName, String registryName)
     {
-    	NAME = unlocalizedName;
+    	name = unlocalizedName;
         this.setUnlocalizedName(unlocalizedName);
     }
     
