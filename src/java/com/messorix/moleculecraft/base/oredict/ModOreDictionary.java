698b146c298748784dc7094a23bf51f018280615
Anime Quatroctus <kanadetachibona@gmail.com>
2016-08-14 12:24:44 -0700

    OreDictionary Added Charcoal to OreDictionary and changed how it is called.

--- src/java/com/messorix/moleculecraft/base/oredict/ModOreDictionary.java
+++ src/java/com/messorix/moleculecraft/base/oredict/ModOreDictionary.java
@@ -1,33 +1,35 @@
 package com.messorix.moleculecraft.base.oredict;
 
 import com.anime.basic.registry.RegistryHelper;
-import com.messorix.moleculecraft.base.ModBlocks;
-import com.messorix.moleculecraft.base.ModItems;
+import com.messorix.moleculecraft.base.init.ModBlocks;
+import com.messorix.moleculecraft.base.init.ModItems;
 
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 public class ModOreDictionary {
 	
-	public ModOreDictionary() {
+	public void applyOreDictionary() {
 		addOresToOreDictionary();
 		addIngotsToOreDictionary();
 		addDustsToOreDictionary();
+		addMiscToOreDictionary();
 	}
 	
-	public void addOresToOreDictionary() {
+	private void addOresToOreDictionary() {
 		RegistryHelper.addOreToOreDict(ModBlocks.ACANTHITE_ORE, "Silver");
 		RegistryHelper.addOreToOreDict(ModBlocks.CASSITERITE_ORE, "Tin");
 		RegistryHelper.addOreToOreDict(ModBlocks.CHALCOCITE_ORE, "Copper");
 		
 	}
 	
-	public void addIngotsToOreDictionary() {
+	private void addIngotsToOreDictionary() {
 		RegistryHelper.addIngotToOreDict(ModItems.COPPER_INGOT, "Copper");
 		RegistryHelper.addIngotToOreDict(ModItems.SILVER_INGOT, "Silver");
 		RegistryHelper.addIngotToOreDict(ModItems.TIN_INGOT, "Tin");
 	}
 	
-	public void addDustsToOreDictionary() {
+	private void addDustsToOreDictionary() {
 		RegistryHelper.addDustToOreDict(ModItems.COPPER_DUST, "Copper");
 		RegistryHelper.addDustToOreDict(ModItems.SILVER_DUST, "Silver");
 		RegistryHelper.addDustToOreDict(ModItems.TIN_DUST, "Tin");
@@ -37,8 +39,9 @@ public class ModOreDictionary {
 		RegistryHelper.addDustToOreDict(ModItems.DIAMOND_DUST, "Diamond");
 	}
 	
-	public void addMiscToOreDictionary() {
+	private void addMiscToOreDictionary() {
 		RegistryHelper.registerObjectToOreDict(Items.COAL, "coal");
+		RegistryHelper.registerObjectToOreDict(new ItemStack(Items.COAL, 1, 1), "coal");
 	}
 	
 }
