477dd1c97bd8a278e617549806d93124442ed6ce
Messorix <itontherun@hotmail.com>
2016-08-19 12:09:29 +0200

    Changed recipe battery

--- src/java/com/messorix/moleculecraft/base/ModRecipes.java
+++ src/java/com/messorix/moleculecraft/base/ModRecipes.java
@@ -7,9 +7,9 @@ public class ModRecipes
     {
 		RecipeHelper.addSmeltingRecipe(ModItems.COPPER_DUST, 0, ModItems.COPPER_INGOT, 1, 0, 0.7F);
 		RecipeHelper.addCraftingRecipe(ModItems.LITHIUM_ION_BATTERY, 1, 0,
-				RecipeHelper.getShapedCrafting(new String[]{"   ", "G L", "   "},
-						new char[]{'G', 'L'},
-						new Object[]{ModItems.GRAPHITE, ModItems.LMO_ELECTRODE}),
+				RecipeHelper.getShapedCrafting(new String[]{"CCC", "G L", "CCC"},
+						new char[]{'G', 'L', 'C'},
+						new Object[]{ModItems.GRAPHITE, ModItems.LMO_ELECTRODE, ModItems.LIPO_CASING}),
 				false);
     }
 }
